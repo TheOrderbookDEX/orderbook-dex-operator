11ccdb45d54eaf0c560b1796d1cc3baff3bb30d5
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2022-11-07 16:30:57 -0300

    add more withdrawERC20 tests

--- test/scenarios/withdrawERC20Scenarios.ts
+++ test/scenarios/withdrawERC20Scenarios.ts
@@ -5,38 +5,34 @@ import { DepositERC20Action } from '../action/DepositERC20Action';
 import { describer } from '../describer/describer';
 import { WithdrawERC20Scenario } from '../scenario/WithdrawERC20Scenario';
 
-// TODO add more withdraw ERC20 tests
 // TODO test withdrawing multiple ERC20
 
 export const withdrawERC20Scenarios = generatorChain(function*() {
+    // test authorized / unathorized withdrawals
+    for (const operatorOwner of [ Account.MAIN, Account.SECOND ]) {
+        for (const caller of [ Account.MAIN, Account.SECOND ]) {
+            yield {
+                describer,
+                caller,
+                operatorOwner,
+                amount: parseValue(1),
+                setupActions: [ new DepositERC20Action({ describer, amount: parseValue(1), operatorOwner }) ],
+                expectedError: operatorOwner != caller ? Unauthorized : undefined,
+            };
+        }
+    }
+
+    // test transfer amount exceeds balance
     yield {
         describer,
         amount: parseValue(1),
+        expectedError: 'ERC20: transfer amount exceeds balance',
+    };
+    yield {
+        describer,
+        amount: parseValue(2),
         setupActions: [ new DepositERC20Action({ describer, amount: parseValue(1) }) ],
-    };
-
-    yield {
-        describer,
-        caller: Account.SECOND,
-        amount: parseValue(1),
-        setupActions: [ new DepositERC20Action({ describer, amount: parseValue(1) }) ],
-        expectedError: Unauthorized,
-    };
-
-    yield {
-        describer,
-        caller: Account.SECOND,
-        operatorOwner: Account.SECOND,
-        amount: parseValue(1),
-        setupActions: [ new DepositERC20Action({ describer, amount: parseValue(1), operatorOwner: Account.SECOND }) ],
-    };
-
-    yield {
-        describer,
-        operatorOwner: Account.SECOND,
-        amount: parseValue(1),
-        setupActions: [ new DepositERC20Action({ describer, amount: parseValue(1), operatorOwner: Account.SECOND }) ],
-        expectedError: Unauthorized,
+        expectedError: 'ERC20: transfer amount exceeds balance',
     };
 
 }).then(function*(properties) {
